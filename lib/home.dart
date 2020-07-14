aa456850b8fb4906d0a7177e7226783955910ee4
77TBear77 <67769672+77TBear77@users.noreply.github.com>
2020-07-13 19:19:43 -0600

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -9,20 +9,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Home')
-          ),
+        appBar: new AppBar(title: new Text('Home')),
         body: new Center(
           child: new Text('This is the home page'),
-        ),
-        floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // will add onPressed code here
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-      ),
-    );
+        ));
   }
 }
-
