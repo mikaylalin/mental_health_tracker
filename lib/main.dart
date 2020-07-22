888d9f4448935b276fc8d8c3f99ec5fc0df0e72c
Mikayla Lin <mikayla.jing.lin@gmail.com>
2020-07-22 03:01:08 -0600

    added links (websites, phone numbers, etc) to resources

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ library my_library;
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-
 //update pubspec.yaml for this to work!
 import 'package:table_calendar/table_calendar.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 //add in files to the library
 part 'graphs.dart';
@@ -18,7 +18,6 @@ part 'home.dart';
 void main() {
   runApp(MyApp());
 }
-
 
 class Quote extends StatefulWidget {
   @override
@@ -60,60 +59,57 @@ class _QuoteState extends State<Quote> {
     "Enlightenment is the key to everything, and it is the key to intimacy, because it is the goal of true authenticity. \n-Marianne Willliamson",
     "We need to find the courage to say no to the things and people that are not serving us if we want to rediscover ourselves and live our lives with authenticity. \n-Barbara de Angelis",
     "I know of nothing more valuable, when it comes to the all-important virtue of authenticity, than simply being who you are. \n-Charles R. Swindoll",
-    ];
+  ];
   Widget build(BuildContext context) {
     //selects an element from the list of quotes
     var element = lst[rnd.nextInt(lst.length)];
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Column(
-            //aligns button to center
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
+        home: Scaffold(
+      body: Center(
+          child: Column(
+              //aligns button to center
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
             Container(
-              //do we want a margin around the button??
-              //margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child:
-              ButtonTheme(
-                //button color
-                buttonColor: Colors.deepPurple,
-                //size of button
-                height: 759.0, minWidth: 700,
-                child: RaisedButton(
-                  //when button is pressed, it calls on the MyBottomNavigatorBar() method
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => MyBottomNavigationBar()));
-                  },
-                  //text inside the method
-                  child: new Text(
-                    element + "\n\n\n\n\n\n Tap to Continue >>>",
-                    //sylistic properties of the text
-                    style: TextStyle(fontSize: 16.0, color: Colors.white, ),
-                    textAlign: TextAlign.center,
+                //do we want a margin around the button??
+                //margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: ButtonTheme(
+              //button color
+              buttonColor: Colors.deepPurple,
+              //size of button
+              height: MediaQuery.of(context).size.height,
+              minWidth: MediaQuery.of(context).size.width,
+              child: RaisedButton(
+                //when button is pressed, it calls on the MyBottomNavigatorBar() method
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              MyBottomNavigationBar()));
+                },
+                //text inside the method
+                child: new Text(
+                  element + "\n\n\n\n\n\n Tap to Continue >>>",
+                  //sylistic properties of the text
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
                   ),
+                  textAlign: TextAlign.center,
                 ),
-              )
-                
-            )
-            ]
-          )
-            
-        ),
-      )
-    );
+              ),
+            ))
+          ])),
+    ));
   }
 }
-
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp (
-
+    return MaterialApp(
       title: 'Mental Health Tracker',
       theme: ThemeData(
         //application theme
@@ -122,14 +118,11 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
 
-
       home: Quote(), //MyBottomNavigationBar(),
       //makes sure we call what's in MyBottomNavigationBar
-
     );
   }
 }
-
 
 class MyBottomNavigationBar extends StatefulWidget {
   // Application home page. It is stateful, meaning
@@ -163,8 +156,6 @@ class _MyBottomNavigationBar extends State<MyBottomNavigationBar> {
       _selectedIndex = index;
     });
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
